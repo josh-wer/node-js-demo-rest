469e61e73a543fabdaeaef1738110717dec941d9
Joshua Uyi <joshuaouyi@gmail.com>
2021-06-15 12:46:50 +0100

    setup bare rest endpoints

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,15 @@
-module.exports.getPosts = (req, res) => {
-    res.send({name:'hohn'})
-}
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [{ title: 'First Post', content: 'This is the first post!' }]
+  });
+};
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  // Create post in db
+  res.status(201).json({
+    message: 'Post created successfully!',
+    post: { id: new Date().toISOString(), title: title, content: content }
+  });
+};
--- routes/feed.js
+++ routes/feed.js
@@ -1,9 +1,13 @@
-const express = require('express')
+const express = require('express');
 
-const feedController = require('../controllers/feed')
+const feedController = require('../controllers/feed');
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/posts', feedController.getPosts)
+// GET /feed/posts
+router.get('/posts', feedController.getPosts);
+
+// POST /feed/post
+router.post('/post', feedController.createPost);
 
 module.exports = router;
